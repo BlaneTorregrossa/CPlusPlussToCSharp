1f5f63257292c35fd05354491614f86ed3500895
BlaneTorregrossa <blanet97@cox.net>
2017-01-05 17:55:27 -0600

    .

---
+++ CPlusPlussToCSharp/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPlusPlussToCSharp
+{
+
+    /*
+     Arrays 1
+              for (int i = 100; i >= 0; i--)
+              {
+                  Console.WriteLine(i);
+              } 
+
+              for (int i = 99; i >= 0; i--)
+              {
+                  Console.WriteLine(i);
+              }
+
+              for (int i = 100; i >= 1; i--)
+              {
+                  Console.WriteLine(i);
+              }
+
+
+
+        
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                int temp = i;
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    i = temp;
+                    Console.WriteLine(i);
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    i = temp;
+                    Console.WriteLine(i);
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    i = temp;
+                    Console.WriteLine(i);
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        
+
+     Arrays 2 (Except it is in a list)
+
+
+
+    */
+
+
+
+    /*  Complete the FizzBuzz assignment but with a List
+        iterate through numbers 0 to 100.
+    - For multiples of 3 print “Fizz”
+    - For multiples of 5 print “Buzz”
+    - For multiples of 3 and 5 print “FizzBuzz”
+    - Otherwise print the number.*/
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //List<string> FizzBuzz = new List<string>();
+
+           // for (int i = 0; i < 100; i++)
+           // {
+           //     int temp = i;
+           //     if (i % 5 == 0 && i % 3 == 0)   // FizzBuzz
+           //     {
+           //         FizzBuzz.Add("FizzBuzz");
+           //     }
+           //     if (i % 3 == 0)                 // Buzz
+           //     {
+           //         FizzBuzz.Add("Fizz");
+           //     }
+           //     if (i % 5 == 0)                 // Fizz
+           //     {
+           //         FizzBuzz.Add("Buzz");
+           //     }
+           //     else                            // Just numbers
+           //     {
+                    
+           //     }
+           // }
+
+        }
+
+
+    }
+}
+
