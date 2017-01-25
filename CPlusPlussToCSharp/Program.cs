51960a823af68a0f72fcffc1509108a55b786c71
BlaneTorregrossa <blanet97@cox.net>
2017-01-25 10:03:50 -0600

    .

--- CPlusPlussToCSharp/Program.cs
+++ CPlusPlussToCSharp/Program.cs
@@ -7,6 +7,136 @@ using System.Threading.Tasks;
 namespace CPlusPlussToCSharp
 {
 
+    //Arrays
+
+    //Arrays 3
+    /*
+           static void Main(string[] args)
+            {
+
+                double[] nums = new double[10];
+                for (int i = 10; i > 0; i--)
+                {
+                    nums[i - 1] = i;
+                }
+
+            }
+
+    
+        //Arrays7 
+        
+        static void Main(string[] args)
+        {
+
+
+            int[,] days = new int[29, 5];
+            //int tempa[1];
+            //int tempb[1];
+            int rowCount = 0;
+            int colCount = 0;
+            int rowSum = 0;
+            int colSum = 0;
+            //int tempa;
+            //int tempb;
+
+            for (int i = 0; i < 29; i++)    // for loop to determine row of array
+            {
+                for (int j = 0; j < 5; j++) // for loop to determine column of array
+                {
+                    days[i, j] = rowCount; // places intergers in the array
+                    rowSum = days[i, j] + rowSum;
+                    rowCount++; // increases numbers by one
+
+                }
+            }
+
+            for (int i = 0; i < 5; i++) // for loop to determine row of array
+            {
+                for (int j = 0; j < 29; j++) // for loop to determine column of array
+                {
+                    days[i, j] = colCount; // places intergers in the array // Error says Index is out of bounds of the array
+                    colSum = days[i, j] + colSum;
+                    colCount++; // increases numbers by one
+
+                }
+            }
+
+
+
+        }
+
+
+
+    }
+
+        //Arrays 8
+        
+        static void Main(string[] args)
+        {
+
+
+
+            int [] health = { 100, 100, 100, 100, 100 };
+            string input;
+            int attackCounter = 0;
+
+            //std::cout << "Please select a player to take damage using keys 0 to 4." << std::endl;
+            //std::cin >> input;
+
+            for (int i = 0; i < 5;)
+            {
+                Console.WriteLine("Please select a player to take damage using keys 0 to 4.");
+                i++;
+
+                if (input > 4 || input < 0)
+                {
+                    Console.WriteLine("No Valid Input")
+                    i--;
+                }
+
+                else if (health[input] <= 0)
+                {
+
+                    Console.WriteLine("Input No longer Valid");
+                    i--;
+                }
+
+                else
+                {
+                    health[input] = health[input] - 40;
+                    attackCounter++;
+                    if (health[input] == -20)
+                    {
+                        health[input] == 0;
+                        Console.WriteLine("20 damage was dealt to your target ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("40 damage was dealt to your target ");
+                    }
+                }
+
+                if (attackCounter == 5)
+                {
+                    Console.WriteLine("Player HP is: ")(health[0])(" HP");
+                    Console.WriteLine("Player HP is: ")(health[2])(" HP");
+                    Console.WriteLine("Player HP is: ")(health[3])(" HP");
+                    Console.WriteLine("Player HP is: ")(health[4])(" HP");
+                    Console.WriteLine("Player HP is: ")(health[5])(" HP");
+                }
+
+            }
+
+
+            return 0;
+        }
+
+
+
+    */
+
+
+
     //Loops
     /*
      Loops 1
@@ -246,7 +376,7 @@ namespace CPlusPlussToCSharp
 
          */
 
- 
+
 
 
     class Program
@@ -255,10 +385,7 @@ namespace CPlusPlussToCSharp
         static void Main(string[] args)
         {
 
-            
-
         }
-
 
 
     }
